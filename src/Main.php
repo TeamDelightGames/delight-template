49299ed09da48ef5d5ef5cf8adb5bcefa9c13256
= <=>
2024-02-18 15:44:40 +0900

    :memo: Add project template-base

---
+++ src/Main.php
@@ -0,0 +1,11 @@
+<?php
+
+declare(strict_types=1);
+
+namespace skh6075\delightgames\template;
+
+use pocketmine\plugin\PluginBase;
+
+final class Main extends PluginBase{
+	public const PREFIX = "§r§l§8 [§cR§8]§r§7 ";
+}
